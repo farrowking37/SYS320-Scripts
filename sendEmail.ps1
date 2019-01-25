edfa65c91886d1492469d33f5605183b8f7e82c1
John Shultz <john.shultz@mymail.champlain.edu>
2019-01-25 11:05:46 -0500

    My First Two Scripts

---
+++ sendEmail.ps1
@@ -0,0 +1,7 @@
+﻿# Send an email
+
+# Create the message body
+$msg = "Hey Adam, `n I'm spamming you because Duane said that I could. Sorry!"
+
+# Create the email structure using the send-MailMessage applet
+Send-MailMessage -From "totally.real.person@snopes.com" -to "agoldstein@champlain.edu" -Subject "I'm learning stuff" -body $msg -SmtpServer 192.168.1.32
