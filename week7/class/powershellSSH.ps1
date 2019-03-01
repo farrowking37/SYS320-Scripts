d5f23b04e11f8b7c07b6cd2ddec7401d70ca8f92
John Shultz <john.shultz@mymail.champlain.edu>
2019-03-01 11:16:05 -0500

    Adding powershellSSH.ps1 to repo. Contains examples of using SSH within powershell using Posh-SSH comands.

--- week7/class/powershellSSH.ps1
+++ week7/class/powershellSSH.ps1
@@ -1,7 +1,13 @@
 ﻿# You may have to run install-module Posh-SSH in order for this script to run.
 
 # Start an SSH Session
+New-SSHsession -Credential (Get-Credential john.shultz) -ComputerName '192.168.1.34'
 
-
+# Run commands on remote host and display output to screen
+(Invoke-SSHCommand -Index 0 "ps -ef").output
 
 # Send file to remote host
+Set-SCPFile -ComputerName '192.168.1.34' -LocalFile "ips-bad-iptables.bash" -RemotePath "/home/john.shultz"
+
+# Check to be sure file uploaded
+(Invoke-SSHCommand -Index 0 "ls -l ips-bad-iptables.bash").output
