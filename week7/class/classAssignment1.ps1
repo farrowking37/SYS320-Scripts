daf941eec67d7d4f9c9196aafd7b4f44bcb20f81
John Shultz <john.shultz@mymail.champlain.edu>
2019-03-01 10:20:52 -0500

    Committing winGet2.ps1 and the first classroom assignment.

---
+++ week7/class/classAssignment1.ps1
@@ -0,0 +1,48 @@
+﻿<#
+
+.SYNOPSIS
+Display items within a given event log, and provide the option to search for a string within if desired.
+
+.PARAMETER Log
+Specify the EventLog that you want to view.
+
+.PARAMETER searchString
+Optional. Specify the the string that you wish to search for in the message body. For best results, wrap in quotes and use an astrisk on either side of the search
+
+.EXAMPLE
+.\classAssignment.ps1 -Log System -searchString "*failure*"
+
+.EXAMPLE
+.\classAssignment.ps1 -l System -s "*failure*"
+
+.EXAMPLE
+.\classAssignment.ps1 -Log System
+
+#>
+
+
+# Params to pass on the commandline
+param (
+
+    # Variables
+    [alias('l')][Parameter(mandatory=$true)][string]$Log,
+    [alias('s')][string]$searchString
+
+)
+
+# Allow for the search string to be mandatory by checking to see if the value is empty 
+# and determining the command to run.
+if ($searchString -eq ""){
+    
+    # Run the command without using the empty $searchString variable
+    Get-EventLog -LogName $Log
+
+
+}
+else {
+
+    # Run the command and process the commandline switches
+    # Use the search string as a searching agent.
+    Get-EventLog -LogName $Log -Message $searchString
+
+}
---
+++ week7/class/winGet2.ps1
@@ -0,0 +1,41 @@
+﻿<#
+
+.SYNOPSIS
+
+Download a file from the Internet
+
+.PARAMETER URL
+
+Specify the URL of a file you wish to retrieve. It is best to enclose in quotes.
+
+
+.PARAMETER saveLocation
+
+Specify the locatoin to save the retrieved file. It is best to enclose in quotes.
+
+.EXAMPLE
+
+.\winGet2.ps1 -URL http://192.168.1.32/cve.csv" -saveLocation "cve2.csv"
+
+.EXAMPLE
+
+.\winGet2.ps1 -u http://192.168.1.32/cve.csv" -s "cve2.csv"
+
+#>
+
+
+# Params to pass to commqandline
+param (
+
+    # Variables
+    [alias("u")][Parameter(mandatory=$true)][string]$URL,
+    [alias("s")][Parameter(mandatory=$true)][string]$saveLocation
+
+)
+
+
+# Run the command and process the commandline switches
+Invoke-WebRequest -URI $URL -OutFile $saveLocation
+
+#Task: Create a new powershell script that uses the "Get-Eventlog" and allows the user to specify
+# an Event Log to view and allow the user to specify a keyword to search within the message"
